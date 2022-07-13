0cb5e5af185cca3ca8f7214fc239cd676347cbd2
Ellian-1 <ellianalba1@gmail.com>
2022-07-13 11:35:33 -0300

    It solves the problem of why the values of the buttons were not shown on the display

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-const display = document.getElementsByClassName('display');
-const buttons = document.querySelectorAll('.button');
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
 
-buttons.forEach(button => button.addEventListener('click', e => display.innerHTML += e.target.innerText));
-
-const operate = (num1, num2) => {
-    
-};
+buttons.forEach(button => button.addEventListener('click', e => display.innerText += e.target.innerText));
