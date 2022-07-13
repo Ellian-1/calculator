027c68dfad058ba26e7503c656a92fd5a88cc9c2
Ellian-1 <ellianalba1@gmail.com>
2022-07-13 11:53:37 -0300

    Customize the resolve

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('button');
 
-buttons.forEach(button => button.addEventListener('click', e => display.innerText += e.target.innerText));
+buttons.forEach(button => button.addEventListener('click', (e) => {
+
+    switch (e.target.innerText) {
+        case 'C':
+            display.innerText = '';
+            break;
+
+        default:
+            display.innerText += e.target.innerText;
+    };
+}));
