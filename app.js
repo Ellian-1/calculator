bf81d4f803ca214f6d17c5d5e3407fb3c7923ebe
Ellian-1 <ellianalba1@gmail.com>
2022-07-20 19:08:08 -0300

    Refactorize code

--- app.js
+++ app.js
@@ -1,82 +1,82 @@
-// These are the variables that store the values of the output and the buttons
-const display = document.querySelector('.display');
-const buttons = document.querySelectorAll('button');
-const equalsButton = document.getElementById('equal');
-const firtstOperator = '';
-const secondOperator = '';
-const mathOperators = document.querySelectorAll('operator');
-const operator = '';
+const calculator = () => {
+    // These are the variables that store the values of the output and the buttons
+    const display = document.querySelector('.display');
+    const buttons = document.querySelectorAll('button');
+    const equalsButton = document.getElementById('equal');
+    let firtstOperator = '';
+    let secondOperator = '';
+    const mathOperators = document.querySelectorAll('operator');
+    let operator = '';
 
-// This is the function to handle the buttons for the operands
-const handleButtons = () => {
-    buttons.forEach(button => button.addEventListener('click', () => {
-        switch (button.innerText) {
-            case 'C': case '=':
-                display.innerText = '';
-                break;
-    
-            default:
-                display.innerText += button.innerText;
-                
-            if (operator === '') {
-                firtstOperator = button.innerText;
-            } else {
-                secondOperator = button.innerText;
+    // Create the function to handle the buttons
+    const handleButtons = () => {
+        buttons.forEach(button => button.addEventListener('click', () => {
+            switch (button.innerText) {
+                case 'C': case '=':
+                    display.innerText = '';
+                    break;
+        
+                default:
+                    display.innerText += button.innerText;
+                    
+                if (operator === '') {
+                    firtstOperator = button.innerText;
+                } else {
+                    secondOperator = button.innerText;
+                };
             };
+        }));
+
+        mathOperators.forEach(mathOperator => mathOperator.addEventListener('click', () => {
+            operator = mathOperator.innerText;
+            operate(firtstOperator, operator, secondOperator);
+        }));
+    };
+    handleButtons();
+
+    // The operate function
+    const operate = (num1, operator, num2) => {
+        
+        num1 = Number(num1);
+        num2 = Number(num2);
+
+        switch (operator.innerText) {
+            case '+':
+                return add(num1, num2);
+
+            case '-':
+                return substract(num1, num2);
+
+            case '*':
+                return multiply(num1, num2);
+
+            case '/':
+                return division(num1, num2);
         };
-    }));
-};
-handleButtons();
+    };
 
-// This is the function to handle the buttons for the operators
-const handleOperators = () => {
-    mathOperators.forEach(mathOperator => mathOperator.addEventListener('click', () => {
-        operator = mathOperator.innerText;
-        operate(firtstOperator, operator, secondOperator);
-    }));
-};
-handleOperators();
+    // The mathematical functions
 
-// The operate function
-const operate = (num1, operator, num2) => {
-    
-    num1 = Number(num1);
-    num2 = Number(num2);
+    const add = (num1, num2) => {
+        const result = num1 + num2;
+        display.textContent = result;
+    };
 
-    switch (operator.innerText) {
-        case '+':
-            return add(num1, num2);
+    const substract = (num1, num2) => {
+        const result = num1 - num2
+        display.textContent = result;
+    };
 
-        case '-':
-            return substract(num1, num2);
+    const multiply = (num1, num2) => {
+        const result = num1 * num2;
+        display.textContent = result;
 
-        case '*':
-            return multiply(num1, num2);
+    };
 
-        case '/':
-            return division(num1, num2);
+    const division = (num1, num2) => {
+        const result = num1 / num2
+        display.textContent = result;
     };
 };
 
-// The mathematical functions
-
-const add = (num1, num2) => {
-    const result = num1 + num2;
-    display.textContent = result;
-};
-
-const substract = (num1, num2) => {
-    const result = num1 - num2
-    display.textContent = result;
-};
-
-const multiply = (num1, num2) => {
-    const result = num1 * num2;
-    display.textContent = result;
-
-};
-
-const division = (num1, num2) => {
-    const result = num1 / num2
-    display.textContent = result;
-};
+calculator();
