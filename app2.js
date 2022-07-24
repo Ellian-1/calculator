fa12b379a0161553db59f3d798e1879c226944a4
Ellian-1 <ellianalba1@gmail.com>
2022-07-24 20:24:36 -0300

    Create extra js code for verification

---
+++ app2.js
@@ -0,0 +1,23 @@
+let display = document.querySelector('.current-display');
+let buttons =Array.from( document.querySelectorAll('.button'));
+
+buttons.map(button => {
+    button.addEventListener('click', e => {
+        switch(e.target.innerText) {
+            case 'C':
+                display.innerText = '';
+                break;
+            
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+
+            default:
+                display.innerText = e.target.innerText;
+        }
+    })
+})
