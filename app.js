0251f2b15e07d1d2fd9b788a7ca8357ca4abbfc3
Ellian-1 <ellianalba1@gmail.com>
2022-07-29 11:02:44 -0300

    Complee the code of the calculator

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-let display = document.getElementsByClassName('display');
-let buttons = Array.from( document.getElementsByClassName('button'));
+let display = document.querySelector('.display');
+let buttons = document.querySelectorAll('.button');
 
-buttons.map(button => {
-    button.addEventListener('click', e => {
-        switch(e.target.innerText) {
-            case 'C':
-                display.innerText = '';
-                break;
-            
-            case '=':
-                try{
-                    console.log(display.innerText);
-                    display.innerText = eval(display.innerText);
-                } catch {
-                    display.innerText = "Error"
-                }
-                break;
+buttons.forEach(button => button.addEventListener('click', () => {
+    switch(button.innerText) {
+        case 'C':
+            display.innerText = '';
+            break;
+        
+        case '=':
+            try{
+                console.log(display.innerText);
+                display.innerText = eval(display.innerText);
+            } catch {
+                display.innerText = "Error"
+            }
+            break;
 
-            default:
-                display.innerText = e.target.innerText;
-        }
-    })
-})
+        default:
+            display.innerText += button.innerText;
+    };
+}))
